4f1ad214bad21d70dd1d4503d5f38af6bd187c6d
luolong <luolong@beyondbit.com>
2021-05-28 16:57:49 +0800

    修复m-ui引用 core 编译失败

--- packages/m-ui/packages/choose/types.ts
+++ packages/m-ui/packages/choose/types.ts
@@ -1,4 +1,4 @@
-import { User } from '@belvoly-vue-aioa/core/services/userService'
+import { User } from '@belvoly-vue-aioa/m-core/services/userService'
 
 export interface PeopleDataFilterOfChoosePeopleOrOrg {
     (users: User[]): User[]
