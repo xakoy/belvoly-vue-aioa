36ca3b17dc788252f49f74f32375ccd0ef9d5146
luolong <luolong@beyondbit.com>
2021-10-22 15:15:17 +0800

    @belvoly-vue-aioa/ui 0.1.49

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,8 @@ setGloablConfig({
     }
 })
 setMobileGloablConfig({
-    apiHost: 'http://192.168.22.22:2001/api',
-    token: 'b6fd5d4aa81d46acb7402e9fa7fabb15',
+    apiHost: 'http://192.168.24.111/api',
+    token: 'f69d5b5c5ec946bdb58f30fa12c1edfa',
     o365: { enabled: true, baseURI: '', blobURI: '', supportFileExtensions: [] },
     wxwork: { enabled: true, debug: true, appId: 'wwc11ee87e20b78040', getJssdkTicketApiUrl: 'http://192.168.101.135:2001/sso/wework/jsapi/signature' }
 })
